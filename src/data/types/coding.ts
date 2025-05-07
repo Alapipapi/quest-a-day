95175b6b9699a6c952abec0a97dd86f7e71dad5c
Alapipapi <m-11369051@moe-dl.edu.my>
2025-05-07 13:54:27 +0800

    Update coding.ts

--- src/data/types/coding.ts
+++ src/data/types/coding.ts
@@ -3,7 +3,5 @@ import { StepDetails } from "./common";
 
 export interface CodingChallenge extends StepDetails {
   category: "coding";
-  difficulty?: "Easy" | "Medium" | "Hard";
-  estimatedTime?: string;
   tools?: string[];
 }
