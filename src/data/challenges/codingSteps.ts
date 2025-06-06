314fd40afe636f115316fdf2c377550fa8927f15
Alapipapi <m-11369051@moe-dl.edu.my>
2025-06-06 14:52:46 +0800

    Update codingSteps.ts

--- src/data/challenges/codingSteps.ts
+++ src/data/challenges/codingSteps.ts
@@ -2,18 +2,80 @@
 import { CodingChallenge } from "../types";
 import { weatherAppChallenge } from "./coding/weatherApp";
 import { todoAppChallenge } from "./coding/todoApp";
+import { apiClientChallenge } from "./coding/apiClient";
 import { portfolioWebsiteChallenge } from "./coding/portfolioWebsite";
+import { authSystemChallenge } from "./coding/authSystem";
+import { progressiveWebAppChallenge } from "./coding/progressiveWebApp";
+import { markdownEditorChallenge } from "./coding/markdownEditor";
+import { kanbanBoardChallenge } from "./coding/kanbanBoard";
+import { dataVisualizationChallenge } from "./coding/dataVisualization";
+import { responsivePortfolioChallenge } from "./coding/responsivePortfolio";
+import { realtimeChatAppChallenge } from "./coding/realtimeChatApp";
+import { apiRateLimiterChallenge } from "./coding/apiRateLimiter";
+import { responsiveDesignChallenge } from "./coding/responsiveDesign";
+import { darkModeToggleChallenge } from "./coding/darkModeToggle";
+import { cssAnimationShowcaseChallenge } from "./coding/cssAnimationShowcase";
+import { formValidationHelperChallenge } from "./coding/formValidationHelper";
+import { chatInterfaceChallenge } from "./coding/chatInterface";
+import { responsiveDashboardChallenge } from "./coding/responsiveDashboard";
+import { mobileFirstWebsiteChallenge } from "./coding/mobileFirstWebsite";
+import { responsiveEmailTemplateChallenge } from "./coding/responsiveEmailTemplate";
+import { microanimationsChallenge } from "./coding/microanimations";
 import { reactHooksChallenge } from "./coding/reactHooks";
 import { responsiveDesignChallenge } from "./coding/responsiveDesign";
 import { stateManagementChallenge } from "./coding/stateManagement";
+import { databaseDesignChallenge } from "./coding/databaseDesign";
+import { microservicesArchitectureChallenge } from "./coding/microservicesArchitecture";
+import { graphqlIntegrationChallenge } from "./coding/graphqlIntegration";
+import { mobileAppIntegrationChallenge } from "./coding/mobileAppIntegration";
+import { automatedTestingChallenge } from "./coding/automatedTesting";
+import { cicdPipelineChallenge } from "./coding/cicdPipeline";
+import { accessibilityAuditChallenge } from "./coding/accessibilityAudit";
+import { securityAuditImplementationChallenge } from "./coding/securityAuditImplementation";
+import { apiDocChallenge } from "./coding/apiDoc";
+import { databaseSchemaOptimizationChallenge } from "./coding/databaseSchemaOptimization";
+import { quickCalculatorWidgetChallenge } from "./coding/quickCalculatorWidget";
+import { colorPickerComponentChallenge } from "./coding/colorPickerComponent";
+import { recipeManagerAppChallenge } from "./coding/recipeManagerApp";
 import { taskManagerAppChallenge } from "./coding/taskManagerApp";
 
 export const codingSteps: CodingChallenge[] = [
   weatherAppChallenge,
   todoAppChallenge,
+  apiClientChallenge,
   portfolioWebsiteChallenge,
+  authSystemChallenge,
+  progressiveWebAppChallenge,
+  markdownEditorChallenge,
+  kanbanBoardChallenge,
+  dataVisualizationChallenge,
+  responsivePortfolioChallenge,
+  realtimeChatAppChallenge,
+  apiRateLimiterChallenge,
+  responsiveDesignChallenge,
+  darkModeToggleChallenge,
+  cssAnimationShowcaseChallenge,
+  formValidationHelperChallenge,
+  chatInterfaceChallenge,
+  responsiveDashboardChallenge,
+  mobileFirstWebsiteChallenge,
+  responsiveEmailTemplateChallenge,
+  microanimationsChallenge,
   reactHooksChallenge,
   responsiveDesignChallenge,
   stateManagementChallenge,
+  databaseDesignChallenge,
+  microservicesArchitectureChallenge,
+  graphqlIntegrationChallenge,
+  mobileAppIntegrationChallenge,
+  automatedTestingChallenge,
+  cicdPipelineChallenge,
+  accessibilityAuditChallenge,
+  securityAuditImplementationChallenge,
+  apiDocChallenge,
+  databaseSchemaOptimizationChallenge,
+  quickCalculatorWidgetChallenge,
+  colorPickerComponentChallenge,
+  recipeManagerAppChallenge,
   taskManagerAppChallenge
 ];
