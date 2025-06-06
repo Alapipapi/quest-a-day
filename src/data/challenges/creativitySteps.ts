9b243d184922672ad50ba053ca3b26a2350d8558
Alapipapi <m-11369051@moe-dl.edu.my>
2025-06-06 14:38:56 +0800

    Update creativitySteps.ts

--- src/data/challenges/creativitySteps.ts
+++ src/data/challenges/creativitySteps.ts
@@ -3,10 +3,42 @@ import { CreativityChallenge } from "../types";
 import { dailySketchChallenge } from "./creativity/dailySketch";
 import { digitalArtChallenge } from "./creativity/digitalArt";
 import { watercolorChallenge } from "./creativity/watercolor";
+import { abstractArtChallenge } from "./creativity/abstractArt";
+import { mobileAppUIChallenge } from "./creativity/mobileAppUI";
+import { naturePhotographyChallenge } from "./creativity/naturePhotography";
+import { musicCreationChallenge } from "./creativity/musicCreation";
+import { foodPhotographyChallenge } from "./creativity/foodPhotography";
 import { speedSketchChallenge } from "./creativity/speedSketch";
 import { storyWritingChallenge } from "./creativity/storyWriting";
+import { voiceActingChallenge } from "./creativity/voiceActing";
+import { visualStorytellingChallenge } from "./creativity/visualStorytelling";
+import { characterDesignChallenge } from "./creativity/characterDesign";
 import { poetryWritingChallenge } from "./creativity/poetryWriting";
+import { diyCraftChallenge } from "./creativity/diyCraft";
+import { urbanSketchingChallenge } from "./creativity/urbanSketching";
+import { animationCreationChallenge } from "./creativity/animationCreation";
+import { podcastProductionChallenge } from "./creativity/podcastProduction";
+import { photoStoryChallenge } from "./creativity/photoStory";
 import { storyboardCreationChallenge } from "./creativity/storyboardCreation";
+import { musicCoverReinterpretationChallenge } from "./creativity/musicCoverReinterpretation";
+import { minimalistPhotographyChallenge } from "./creativity/minimalistPhotography";
+import { comicStripCreationChallenge } from "./creativity/comicStripCreation";
+import { shortFilmStoryboardingChallenge } from "./creativity/shortFilmStoryboarding";
+import { mixedMediaArtChallenge } from "./creativity/mixedMediaArt";
+import { songwritingChallenge } from "./creativity/songwritingChallenge";
+import { digitalStorytellingProjectChallenge } from "./creativity/digitalStorytellingProject";
+import { colorTheoryArtChallenge } from "./creativity/colorTheoryArt";
+import { soundDesignWorkshopChallenge } from "./creativity/soundDesignWorkshop";
+import { mixedMediaCollageChallenge } from "./creativity/mixedMediaCollage";
+import { penAndInkChallenge } from "./creativity/penAndInk";
+import { creativeWritingPromptChallenge } from "./creativity/creativeWritingPrompt";
+import { foundObjectSculptureChallenge } from "./creativity/foundObjectSculpture";
+import { botanicalDrawingChallenge } from "./creativity/botanicalDrawing";
+import { storyboardAnimationChallenge } from "./creativity/storyboardAnimation";
+import { speedSketchChallenge } from "./creativity/speedSketch";
+import { colorPaletteCreationChallenge } from "./creativity/colorPaletteCreation";
+import { microPoetryChallenge } from "./creativity/microPoetry";
+import { digitalPortraitStudyChallenge } from "./creativity/digitalPortraitStudy";
 import { musicCreationChallenge } from "./creativity/musicCreation";
 import { mobileAppUIChallenge } from "./creativity/mobileAppUI";
 import { brandIdentityPackageChallenge } from "./creativity/brandIdentityPackage";
@@ -18,14 +50,47 @@ export const creativitySteps: CreativityChallenge[] = [
   dailySketchChallenge,
   digitalArtChallenge,
   watercolorChallenge,
+  abstractArtChallenge,
+  mobileAppUIChallenge,
+  naturePhotographyChallenge,
+  musicCreationChallenge,
+  foodPhotographyChallenge,
   speedSketchChallenge,
   storyWritingChallenge,
+  voiceActingChallenge,
+  visualStorytellingChallenge,
+  characterDesignChallenge,
   poetryWritingChallenge,
+  diyCraftChallenge,
+  urbanSketchingChallenge,
+  animationCreationChallenge,
+  podcastProductionChallenge,
+  photoStoryChallenge,
   storyboardCreationChallenge,
+  musicCoverReinterpretationChallenge,Add commentMore actions
+  minimalistPhotographyChallenge,
+  comicStripCreationChallenge,
+  shortFilmStoryboardingChallenge,
+  mixedMediaArtChallenge,
+  songwritingChallenge,
+  digitalStorytellingProjectChallenge,
+  colorTheoryArtChallenge,
+  soundDesignWorkshopChallenge,
+  mixedMediaCollageChallenge,
+  penAndInkChallenge,
+  creativeWritingPromptChallenge,
+  foundObjectSculptureChallenge,
+  botanicalDrawingChallenge,
+  storyboardAnimationChallenge,
+  speedSketchChallenge,
+  colorPaletteCreationChallenge,
+  microPoetryChallenge,
+  digitalPortraitStudyChallenge,
+  brandIdentityPackageChallenge,
   musicCreationChallenge,
   mobileAppUIChallenge,
   brandIdentityPackageChallenge,
   naturePhotographyChallenge,
   personalBrandDesignChallenge,
-  podcastSeriesCreationChallenge
+  podcastSeriesCreationChallenge,
 ];
